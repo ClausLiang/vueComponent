174be1d667b4e46fdc4b32b411038ce274b61f32
liangyonggang <1475524294@qq.com>
2018-08-13 00:50:35 +0800

    Initial commit

---
+++ src/components/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="index-wrapper">
+    <button class="toast-btn">modal</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped lang="stylus">
+  .index-wrapper{
+    height: 100%;
+    .toast-btn{
+      width: 50px;
+      height: 25px;
+      border: 1px solid #00ff00;
+      margin: 10px;
+    }
+  }
+</style>
