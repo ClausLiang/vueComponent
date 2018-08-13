6a37137f879245438b451e1a60f690183cb23957
liangyonggang <yonggangliang@sina.cn>
2018-08-13 17:18:29 +0800

    倒计时组件

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="index-wrapper">
+    <div class="count-wrapper">
+      <CountDown :startTime="startTime" :endTime="endTime"></CountDown>
+    </div>
     <button class="toast-btn" @click="showModal">modal</button>
     <Modal ref="modal" content="您还未参与线下预约，请联系案场专员。"></Modal>
   </div>
@@ -7,12 +10,19 @@
 
 <script>
 import Modal from './modal/Modal'
+import CountDown from './countDown/CountDown'
 export default {
   name: 'index',
-  components: {Modal},
+  components: {Modal, CountDown},
   data () {
     return {
+      startTime: '',
+      endTime: ''
     }
+  },
+  created () {
+    this.startTime = new Date('2018-08-13 00:00:00')
+    this.endTime = new Date('2018-08-13 07:22:24')
   },
   methods: {
     showModal () {
@@ -30,6 +40,10 @@ export default {
       height: 25px;
       border: 1px solid #00ff00;
       margin: 10px;
+    }
+    .count-wrapper{
+      margin: 10px;
+      display: inline-block;
     }
   }
 </style>
---
+++ src/components/countDown/CountDown.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="count-down">
+    <span v-show="countInfo.hour0 !== 0">{{countInfo.hour0}}</span>
+    <span>{{countInfo.hour1}}</span>
+    <span>{{countInfo.hour2}}</span>
+    <em>:</em>
+    <span>{{countInfo.min1}}</span>
+    <span>{{countInfo.min2}}</span>
+    <em>:</em>
+    <span>{{countInfo.ss1}}</span>
+    <span>{{countInfo.ss2}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'count-down',
+  data () {
+    return {
+      countInfo: {
+        hour0: 0,
+        hour1: 0,
+        hour2: 0,
+        min1: 0,
+        min2: 0,
+        ss1: 0,
+        ss2: 0
+      },
+      differ: 0
+    }
+  },
+  props: ['startTime', 'endTime'],
+  mounted () {
+    this.differ = this.endTime - this.startTime
+    this.setCountDown(this.differ)
+    setInterval(() => {
+      this.differ = this.differ - 1000
+      this.setCountDown()
+    }, 1000)
+  },
+  methods: {
+    setCountDown () {
+      const hour = parseInt(this.differ / (60 * 60 * 1000)) // 先算有多少小时
+      if (hour > 99) {
+        this.countInfo.hour0 = parseInt(hour / 100)
+      } else if (hour > 9) {
+        this.countInfo.hour1 = parseInt(hour / 10)
+        this.countInfo.hour2 = hour % 10
+      } else {
+        this.countInfo.hour1 = 0
+        this.countInfo.hour2 = hour
+      }
+      const minute = parseInt((this.differ % (60 * 60 * 1000)) / (60 * 1000)) // 多少分钟
+      if (minute > 9) {
+        this.countInfo.min1 = parseInt(minute / 10)
+        this.countInfo.min2 = minute % 10
+      } else {
+        this.countInfo.min1 = 0
+        this.countInfo.min2 = minute
+      }
+      const second = parseInt((this.differ % (60 * 1000)) / 1000) // 多少秒
+      if (second > 9) {
+        this.countInfo.ss1 = parseInt(second / 10)
+        this.countInfo.ss2 = second % 10
+      } else {
+        this.countInfo.ss1 = 0
+        this.countInfo.ss2 = second
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  .count-down{
+    display: flex;
+    span{
+      display: inline-block;
+      margin-right: 6px;
+      width: 24px;
+      height: 24px;
+      line-height: 24px;
+      text-align: center;
+      background: #fda90b;
+      color: #fff;
+      border-radius: 5px;
+      font-size: 15px;
+      &:last-child{
+        margin-right: 0;
+      }
+    }
+    em{
+      margin-right: 6px;
+      line-height: 24px;
+      font-size: 15px;
+      font-style: normal;
+      font-weight: bold;
+    }
+  }
+</style>
